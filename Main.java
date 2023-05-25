3af25252cb0c2548d70382923433b5f1d9283719
resty000 <128879285+resty000@users.noreply.github.com>
2023-05-25 17:32:48 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package latmod5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Binary pohon = new Binary();
+
+        pohon.NewNode(20);
+        pohon.NewNode(2);
+        pohon.NewNode(25);
+        pohon.NewNode(37);
+        pohon.NewNode(16);
+
+        System.out.println("\n Pre Order : ");
+        pohon.preOrder(pohon.root);
+        System.out.println("\n In Order : ");
+        pohon.inOrder(pohon.root);
+        System.out.println("\n Post Order : ");
+        pohon.postOrder(pohon.root);
+
+    }
+
+}
